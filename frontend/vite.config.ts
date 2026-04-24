d91002de67027cf3d1a93e9421af11defcfa0adf
Admin <admin@example.com>
2026-04-24 14:32:23 +0530

    ff

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig(async () => {
       chunkSizeWarningLimit: 1000,
       rollupOptions: {
         output: {
-          manualChunks(id) {
+          manualChunks(id: string) {
             if (id.includes('node_modules')) {
               return id.toString().split('node_modules/')[1].split('/')[0].toString();
             }
